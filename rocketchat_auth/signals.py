9b12c0c1878bfd34c6dac2f55e018fe1bacc4c45
Rodrigo Orem <rodrigo@prazocerto.com.br>
2017-06-02 21:26:07 -0300

    first try with post_save signal

--- rocketchat_auth/signals.py
+++ rocketchat_auth/signals.py
@@ -1,12 +1,18 @@
 from django.contrib.auth.signals import user_logged_out
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 from django.dispatch import receiver
-
-from pymongo import MongoClient
 from django.conf import settings
+from pymongo import MongoClient
+import requests
+import logging
 
+
+logger = loggin.getLogger('dm.log')
 
 @receiver(user_logged_out)
 def logout(sender, user, request, **kwargs):
+    logger.error('teste')
     client = MongoClient("mongodb://" + settings.MONGO_DB)
     mongo = client.rocketchat
 
@@ -16,3 +22,9 @@ def logout(sender, user, request, **kwargs):
             '$set': {'services': {'iframe': {'token': ''}}}
         }
     )
+
+
+@receiver(post_save, sender=User)
+def update_user(sender, **kwargs):
+    logger.error('usr updated')
+    print(update_fields)
